68c644e03078ac8c207cb5df8c45870f6ce36385
Maria Mueller <mariafrancesca139@gmail.com>
2016-11-05 23:12:20 -0700

    Add files via upload

---
+++ Molecule.java
@@ -0,0 +1,81 @@
+/*
+ * Maria Mueller
+ * Math 126, Journal 3
+ * November 4, 2016
+ * 
+ * Creates molecule object, which holds ratio of atoms in it
+ */
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Molecule implements Comparable<Molecule>{
+        private Map<Atom, Integer> atoms;
+        private String name;
+        
+        public Molecule(String formula) {
+            this.atoms = new TreeMap<Atom, Integer>();
+            this.name = formula;
+            int start = 0, cursor = 1; String segment;
+            while (cursor < formula.length()) {
+                if (Character.isUpperCase(formula.charAt(cursor))) {
+                    segment = formula.substring(start, cursor);
+                    this.insertAtom(segment);
+                    start = cursor;
+                }
+                cursor++;
+            }
+            if (cursor != start) {
+                segment = formula.substring(start, cursor);
+                this.insertAtom(segment);
+            }       
+        }
+        @Override
+        public String toString() {
+            String result = this.atoms.toString();
+            return result;
+        }
+        
+        public Map<Atom, Integer> getAtoms() {
+            return new TreeMap<Atom, Integer>(this.atoms);
+        }
+        public String getName() {
+            return this.name;
+        }
+        
+        public boolean contains(Atom a) {
+            for (Atom x: atoms.keySet()) {
+                if (a.equals(x)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+        
+        public int get(Atom a) {
+            return this.atoms.get(a);
+        }
+        
+        public void insertAtom(String x) {
+            int counter = 0; int value = 1;
+            while (counter < x.length()) {
+                if (Character.isDigit(x.charAt(counter))) {
+                    value = Integer.parseInt(x.substring(counter));
+                    break;
+                }
+                counter++;
+            }
+            String name = x.substring(0,  counter);
+            this.atoms.put(new Atom(name), value);
+        }
+        
+        @Override
+        public int compareTo(Molecule other){
+            return this.getName().compareTo(other.getName());
+            
+        }
+        
+        public boolean equals(Molecule other) {
+            return this.getName().equals(other.getName());                    
+        }
+}
